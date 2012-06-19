e3f2ca601bf96ead6ef044870ba39e7573824e94
Scott M. Kroll <skroll@gmail.com>
2012-06-19 16:11:55 -0400

    Add documentation to syscron class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,23 @@
+# == Class: syscron
+#
+# System level crontab support for a node.
+#
+# === Parameters
+#
+# === Variables
+#
+# === Examples
+#
+#  class { 'syscron': }
+#
+# === Authors
+#
+# Scott Kroll <skroll@gmail.com>
+#
+# === Copyright
+#
+# Copyright 2012 Scott Kroll, unless otherwise noted.
+#
 class syscron (
 ) {
   include concat::setup
